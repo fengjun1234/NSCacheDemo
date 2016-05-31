883c209e4c346a5926c292037218e6ab835396a2
fengjun1234 <fj_fengjun@126.com>
2016-05-31 21:55:09 +0800

    编写,测试完成

--- 7-Cache演示/ViewController.m
+++ 7-Cache演示/ViewController.m
@@ -8,20 +8,58 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<NSCacheDelegate>
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSCache *_myCache;
+    
+    
+}
+- (IBAction)showCache:(id)sender {
+    
+    NSLog(@"_myCache中当前内容:");
+    
+    for (NSInteger i = 0; i < 20; i++) {
+        NSLog(@"%@",[_myCache objectForKey:@(i)]);
+    }
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _myCache = [[NSCache alloc] init];
+    _myCache.countLimit = 10;
+    
+    
+    _myCache.delegate = self;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    //向cache中添加20个对象
+    
+    
+    
+    for (NSInteger i = 0; i < 20; i++) {
+        
+        NSString * str = [@"object" stringByAppendingString:@(i).description  ];
+        
+        
+        [_myCache setObject:str forKey:@(i)];
+         NSLog(@"然后添加了==>%@",str);
+    }
+    
+    
+   
 }
 
+- (void)cache:(NSCache *)cache willEvictObject:(id)obj{
+    
+    NSLog(@"先删除了==>%@",obj);
+    
+    
+}
 @end
